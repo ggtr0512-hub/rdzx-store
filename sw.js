69f8d3c7037f8d6e3431c1cc662e52c34800b1e1
RDZX STORE <COLOQUE_SEU_EMAIL_DO_GITHUB_AQUI>
2026-09-17 15:50:25 -0300

    Publicar configuracao do aplicativo instalavel

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE = "rdzx-store-v1";
+
+self.addEventListener("install", function(event){
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", function(event){
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", function(event){
+  event.respondWith(
+    fetch(event.request).catch(function(){
+      return caches.match(event.request);
+    })
+  );
+});
